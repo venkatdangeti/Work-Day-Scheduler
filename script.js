ba8d793f5d9568a564d71db8c929920d01b4b74c
Venkatdangeti <dangeti.venkat@gmail.com>
2024-01-21 20:17:32 +0000

    Added time block hours and compared with actual hours to script file

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ var weekdayName = [
   "Saturday"
 ];
 
-
+//ready function
 $(document).ready(function () {
   //day.js code for current date and time
   let NowMoment = dayjs().format("MMMM D, YYYY h:mm A");
@@ -40,4 +40,21 @@ $(document).ready(function () {
   displayDate.innerHTML = NowMoment;
   let currentHour = dayjs().format("HH");
 
+});
+
+
+//get hour from each time block ad compares it to actual time
+$(".time-div").each(function () {
+  var timeDiv = $(this).attr("id").split("-")[1];
+
+  if (currentHour == timeDiv) {
+    $(this).addClass("present");
+    $(this).children(".description").addClass("white-text");
+  } else if (currentHour < timeDiv) {
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  } else if (currentHour > timeDiv) {
+    $(this).removeClass("future");
+    $(this).addClass("past");
+  }
 });
