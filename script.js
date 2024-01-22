a41ba076fbc2578b90859ff0c7c7456565097042
Venkatdangeti <dangeti.venkat@gmail.com>
2024-01-22 15:26:17 +0000

    Added few fixes to the index, script and style files

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
 
-//variables
-var weekday = "";
-var day = 0;
-var month = "";
-var hour = 0;
-
-//arrays with all month and weekday names
-
-var monthName = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-
-var weekdayName = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-
 //ready function
 $(document).ready(function () {
   //day.js code for current date and time
@@ -40,21 +7,20 @@ $(document).ready(function () {
   displayDate.innerHTML = NowMoment;
   let currentHour = dayjs().format("HH");
 
-});
 
+  //get hour from each time block and compares it to actual time
+  $(".time-div").each(function () {
+    var timeDiv = $(this).attr("id").split("-")[1];
 
-//get hour from each time block and compares it to actual time
-$(".time-div").each(function () {
-  var timeDiv = $(this).attr("id").split("-")[1];
-
-  if (currentHour == timeDiv) {
-    $(this).addClass("present");
-    $(this).children(".description").addClass("white-text");
-  } else if (currentHour < timeDiv) {
-    $(this).removeClass("present");
-    $(this).addClass("future");
-  } else if (currentHour > timeDiv) {
-    $(this).removeClass("future");
-    $(this).addClass("past");
-  }
+    if (currentHour == timeDiv) {
+      $(this).addClass("present");
+      $(this).children(".description").addClass("white-text");
+    } else if (currentHour < timeDiv) {
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    } else if (currentHour > timeDiv) {
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+  });
 });
