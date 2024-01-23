82835aab1c809025baf5cfcf35ebd14a07125d6a
Venkatdangeti <dangeti.venkat@gmail.com>
2024-01-23 09:09:03 +0000

    Added retreive function to script and minor fixes to index and style files

--- script.js
+++ script.js
@@ -33,4 +33,14 @@ $(document).ready(function () {
   });
 
 
+  // retrieve data from local storage and sets them in proper places
+  $("#hour-09 .time-block").val(localStorage.getItem("09"));
+  $("#hour-10 .time-block").val(localStorage.getItem("10"));
+  $("#hour-11 .time-block").val(localStorage.getItem("11"));
+  $("#hour-12 .time-block").val(localStorage.getItem("12"));
+  $("#hour-13 .time-block").val(localStorage.getItem("13"));
+  $("#hour-14 .time-block").val(localStorage.getItem("14"));
+  $("#hour-15 .time-block").val(localStorage.getItem("15"));
+  $("#hour-16 .time-block").val(localStorage.getItem("16"));
+  $("#hour-17 .time-block").val(localStorage.getItem("17"));
 });
